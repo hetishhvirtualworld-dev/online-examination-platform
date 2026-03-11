87f1ec348fb59bcb44cdbea7f06454bd90a4bdab
hetishhvirtualworld-dev <hetishh.virtual.world@gmail.com>
2026-03-11 22:01:25 +0530

    Added Eureka Server

---
+++ EurekServer/src/test/java/com/example/demo/mcroservice/EurekServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.mcroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
